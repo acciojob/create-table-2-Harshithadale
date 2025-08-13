bfe0c374f0f02a9a86a6e94b7f57981930189337
harshithadale <daleharshi6045@gmail.com>
2025-08-13 13:12:32 +0530

    initial-commit

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function createTable() {
     //Write your code here
+    const rn = prompt("Input number of rows")
+    const cn=prompt("Input number of columns")
+    document.getElementById("myTable").innerHTML = "";
+    for(let i = 0;i<rn;i++){
+        let row = `<tr>`
+        for(let j=0;j<cn;j++){
+            row += `Row-${i} Column${j}`
+        }
+        row +=`</tr>`
+        document.getElementById("myTable").innerHTML += row
+    }
   
 }
